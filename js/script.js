8eaafebf2d5f50ed0122110e3bc57c5f992f2644
Veinattt <72652694+Veinattt@users.noreply.github.com>
2020-11-10 23:49:13 +0300

    фывыф

--- js/script.js
+++ js/script.js
@@ -25,25 +25,20 @@ $(document).ready(function () {
         variableWidth: true,
         draggable: false,
         asNavFor: '.slider-nav',
-        waitForAnimate: false,
+        waitForAnimate:false,
         responsive: [{
-            breakpoint: 992,
-            settings: {
-                arrows: false,
-                draggable: true,
-            }
-        }, ],
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    draggable: true,
+                }
+            },
+        ],
     });
 });
-
-function set_language(n) {
-    var lang = jQuery('#language'+n).val();
-    window.location.href = '?lang='+lang;
-}
-
 $('.slider-nav').slick({
     arrows: false,
-
+    
     slidesToScroll: 1,
     asNavFor: '.slider-team',
     speed: 1000,
@@ -51,17 +46,18 @@ $('.slider-nav').slick({
     variableWidth: true,
     draggable: false,
     focusOnSelect: true,
-    waitForAnimate: false,
+    waitForAnimate:false,
     responsive: [{
         breakpoint: 768,
         settings: {
             slidesToShow: 5,
         }
-    }, ],
+    },
+],
 });
 // $('.slider-navs2').slick({
 //     arrows: false,
-
+    
 //     slidesToScroll: 1,
 //     asNavFor: '.slider-s2',
 //     speed: 1000,
@@ -92,7 +88,7 @@ $(document).ready(function () {
         speed: 1000,
         draggable: true,
         dots: true,
-
+        
     });
 });
 
@@ -109,65 +105,6 @@ function newBlock(n) {
 }
 
 
-function validate_p1() {
-    var name = document.getElementById('FormInputName');
-    var email = document.getElementById('FormInputEmail');
-    var number = document.getElementById('FormInputNumber');
-
-    if (is_valid(name) && validateEmail(email) && is_valid(number)) {
-        display_p2(0);
-    }
-}
-
-function validate_p2() {
-    var f_address = document.getElementById('FormInputAddress');
-    var d_address = document.getElementById('FormInputDeliveryAddress');
-    var cargo = document.getElementById('FormInputCargo');
-    var output = false;
-    if (is_valid(f_address) && is_valid(d_address) && is_valid(cargo)) {
-        output = true;
-    } 
-    return output;
-}
-
-function validate_p3() {
-    var name = document.getElementById('FormInputName2');
-    var email = document.getElementById('FormInputEmail2');
-    var number = document.getElementById('FormInputNumber2');
-    var output = false;
-    if (is_valid(name) && validateEmail(email) && is_valid(number)) {
-        output = true;
-    }
-    return output;
-}
-
-function validateEmail(email) {
-    const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    var res = false;
-    if (re.test(String(email.value).toLowerCase())) {
-        res = true;
-    } else {
-        email.focus();
-    };
-    return res;
-}
-
-function is_valid(input) {
-    var res = false;
-    if (input && input.value.trim()) {
-        res = true
-    } else {
-        input.focus();
-    }
-    return res;
-}
-
-$("input").keydown(function (event) {
-    if (event.keyCode == 13) {
-        event.preventDefault();
-    }
-});
-
 function display_p2(n) {
     form = document.getElementById("form1");
     if (n == 0) {
@@ -181,21 +118,21 @@ function openSideNav() {
     var a = document.getElementsByClassName('sideCont');
     document.getElementById("mySidenav").style.width = "100%";
     setTimeout(function () {
-        for (i = 0; i < a.length; i++) {
-            a[i].style["opacity"] = 1;
-        }
+      for (i = 0; i < a.length; i++) {
+        a[i].style["opacity"] = 1;
+      }
     }, 400);
-}
-
-function closeSideNav() {
+  }
+  
+  function closeSideNav() {
     var a = document.getElementsByClassName('sideCont');
     for (i = 0; i < a.length; i++) {
-        a[i].style["opacity"] = 0;
+      a[i].style["opacity"] = 0;
     }
     setTimeout(function () {
-        document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("mySidenav").style.width = "0";
     }, 300);
-}
+  }
 
 document.addEventListener('click', function (e) {
     if (document.activeElement.toString() == '[object HTMLButtonElement]') {
